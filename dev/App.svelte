c0afdd1deaaca87f47ffac0648131a99f60e2a32
Jonatan Lundin <jonatan.lundin@gmail.com>
2019-11-04 19:14:07 +0100

    Formatting

--- dev/App.svelte
+++ dev/App.svelte
@@ -1,13 +1,14 @@
 <script>
   import Shortcodes from "../src/Shortcodes.svelte";
   import Heading from "./Heading.svelte";
+
   let markup = `<div>
     <h1>H1 Text</h1>
     [heading]Heading[/heading]
     <p>body</p>
   </div>`;
+
   let components = {
-    // h1: Heading,
     heading: Heading
   };
 </script>
